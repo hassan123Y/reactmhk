e692a336da736a837122ebc6d6dc44adc682c335
Hassanmhk7777 <191459290+Hassanmhk7777@users.noreply.github.com>
2025-12-23 10:02:24 +0100

    TD01 : Ajouter un footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'; // Si tu as gardé le CSS
 import Header from './components/Header';
 import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 function App() {
   return (
     <div className="app-container">
       <Header />
       <MainContent />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+// src/components/Footer.jsx
+function Footer() {
+  return (
+    <footer>
+      <p>Tous droits réservés - DOE John</p>
+    </footer>
+  );
+}
+
+export default Footer;
