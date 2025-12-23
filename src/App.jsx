72d422bfb565c9f526dac2487a2906bbd603f2b0
Hassanmhk7777 <191459290+Hassanmhk7777@users.noreply.github.com>
2025-12-23 10:00:47 +0100

    TD01 : Ajouter un composant pour le contenu principal de la page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import Header from './components/Header';
 import './App.css'; // Si tu as gardé le CSS
-
+import Header from './components/Header';
+import MainContent from './components/MainContent';
 function App() {
   return (
     <div className="app-container">
       <Header />
+      <MainContent />
     </div>
   );
 }
---
+++ src/MainContent.jsx
@@ -0,0 +1,10 @@
+// src/components/MainContent.jsx
+function MainContent() {
+  return (
+    <main>
+      <p>Ici, nous afficherons des informations interessantes :)</p>
+    </main>
+  );
+}
+
+export default MainContent;
