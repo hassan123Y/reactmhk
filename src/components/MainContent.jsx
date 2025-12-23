86fa4492f7305d8cf14c2b31e956b202e7592554
Hassanmhk7777 <191459290+Hassanmhk7777@users.noreply.github.com>
2025-12-23 10:03:10 +0100

    TD02: Afficher la date et l'heure du jour

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,8 +1,25 @@
 // src/components/MainContent.jsx
 function MainContent() {
+  // 1. On récupère la date actuelle en JavaScript (avant le return)
+  const date = new Date();
+
+  // 2. On formate les éléments demandés
+  // Astuce : toLocaleDateString permet d'avoir le format français facilement
+  const dateDuJour = date.toLocaleDateString("fr-FR", {
+    weekday: "long", // Lundi
+    year: "numeric", // 2023
+    month: "long",   // Octobre
+    day: "numeric",  // 23
+  });
+
+  const heureActuelle = date.toLocaleTimeString("fr-FR");
+
+  // 3. On injecte les variables dans le JSX avec des accolades {}
   return (
     <main>
-      <p>Ici, nous afficherons des informations interessantes :)</p>
+      <p>
+        Bonjour, on est le {dateDuJour} et il est {heureActuelle}
+      </p>
     </main>
   );
 }
